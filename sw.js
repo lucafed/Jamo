7334e639f5f7b883b748bd01767db6302d3d24c9
lucafed <luca_fed@libero.it>
2025-12-26 21:52:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,29 @@
 const CACHE = "jamo-v1";
-const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/sw.js"];
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/icons/maskable-512.png"
+];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
